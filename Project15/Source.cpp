7888f5b5e42409c04a66bc883263efe3e44b9cdc
BuildTools <unconfigured@null.spigotmc.org>
2023-04-30 15:19:40 -0400

    added template function and int division functionality

---
+++ Project15/Source.cpp
@@ -0,0 +1,38 @@
+//Luke Scholler
+//CIS 1202
+//April 30, 2023
+
+#include<iostream>
+#include<string>
+#include<fstream>
+#include<cstdlib>
+#include<iomanip>
+#include<cstring>
+
+//template for double and float
+template <typename T>
+T half(T num) {
+	return num / 2;
+}
+
+//overloaded case for int due to rounding issues
+int half(int);
+
+using namespace std;
+
+int main() {
+	double num1 = 7;
+	float num2 = 5;
+	int num3 = 3;
+
+	cout << half(num1) << endl;
+	cout << half(num2) << endl;
+	cout << half(num3) << endl;
+
+	system("pause");
+	return 0;
+}
+
+int half(int num) {
+	return(num / 2 + (num % 2));
+}
